252dd8ea59bc13c56bb8f807db89860b70a9e2b9
JorgeLasaosa <georgelasaosa@gmail.com>
2017-03-17 20:28:08 +0100

    Added Swagger documentatio

--- practica4/server.js
+++ practica4/server.js
@@ -3,6 +3,31 @@ var express = require("express");
     app = express();
     router = express.Router();
     memo = require("./models/memo.js");
+    swaggerJSDoc = require('swagger-jsdoc');
+
+// Swagger definition
+var swaggerDefinition = {
+    "info": {
+        "title" : "API de gestión de notas",
+        "version" : "1.0.0",
+        "description" : "Descripción del API del servicio de notas"
+    },
+    "host" : "localhost:8888",
+    "basePath" : "/"
+};
+
+// Options for Swagger docs
+var options = {
+    // import swaggerDefinitions
+    "swaggerDefinition" : swaggerDefinition,
+    // path to API docs
+    "apis" : ["*.js"]
+}
+
+// Initialize swagger-jsdoc
+var swaggerSpec = swaggerJSDoc(options);
+
+app.use(express.static("public"));
 
 // Accept JSON and URLencoded values
 app.use(bodyParser.json());
@@ -14,9 +39,56 @@ var server = app.listen(8888, function() {
   console.log("Server listening on port %s", server.address().port);
 });
 
-/*
- *  /memos
- */
+// Serve Swagger
+app.get("/swagger.json", function(req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+});
+
+ /**
+  * @swagger
+  * definition:
+  *     Memo:
+  *         properties:
+  *             text:
+  *                 type : string
+  *             deadline:
+  *                 type : date
+  *             file:
+  *                 type : buffer
+  */
+
+  /**
+   * @swagger
+   * /memos:
+   *    get:
+   *        tags:
+   *            - Memos
+   *        description: Returns all memos
+   *        produces:
+   *            - application/json
+   *        responses:
+   *            200:
+   *                description: An array of memos
+   *                schema:
+   *                    $ref: "#/definitions/Memo"
+   *    post:
+   *        tags:
+   *            - Memos
+   *        description: Creates a new memo
+   *        produces:
+   *            - application/json
+   *        parameters:
+   *            - name: memoData
+   *              description: Memo's text, deadline and file to attach
+   *              in: body
+   *              required: true
+   *              schema:
+   *                $ref: '#/definitions/Memo'
+   *        responses:
+   *            200:
+   *                description: Successfully created
+   */
 router.route("/memos")
     .get(function(req, res) {
         var response = {};
@@ -49,8 +121,26 @@ router.route("/memos")
         });
     });
 
-/*
- * /memos/:id
+/**
+ * @swagger
+ * /memos/{id}:
+ *   get:
+ *     tags:
+ *       - Memos
+ *     description: Returns a single memo
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Memo's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: A single memo
+ *         schema:
+ *           $ref: '#/definitions/Memo'
  */
 router.route("/memos/:id")
     .get(function(req, res) {
@@ -65,6 +155,25 @@ router.route("/memos/:id")
             res.json(response);
         });
     })
+    /**
+     * @swagger
+     * /memos/{id}:
+     *   delete:
+     *     tags:
+     *       - Memos
+     *     description: Deletes a single memo
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: id
+     *         description: Memo's id
+     *         in: path
+     *         required: true
+     *         type: integer
+     *     responses:
+     *       200:
+     *         description: Successfully deleted
+     */
     .delete(function(req, res) {
         var response = {};
         memo.remove({_id : req.params.id}, function(err) {
